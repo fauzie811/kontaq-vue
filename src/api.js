6dc9aa3463dc10484f3c985ffc2d94fe4082ba2d
Fauzie <fauzie811@yahoo.com>
2023-05-23 20:03:49 +0700

    feat: .env api url

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import authStore from '@/store/auth';
 
 window.axios = axios;
 
-axios.defaults.baseURL = 'http://kontaq.test/api/';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 // axios.defaults.withCredentials = true;
 // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.interceptors.request.use(
