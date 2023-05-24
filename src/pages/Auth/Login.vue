b8be664f4f20d48946ab0991cabb9a1efadb9dc6
Fauzie <fauzie811@yahoo.com>
2023-05-24 08:03:07 +0700

    fix

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -19,7 +19,7 @@
           <div>
             <form v-on:submit.prevent="doLogin" class="space-y-6">
               <div>
-                <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                <label for="username" class="block text-sm font-medium leading-6 text-gray-900">ID Anggota</label>
                 <div class="mt-2">
                   <input v-model="username" id="username" name="username" type="text" autocomplete="username" required
                     class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6" />
