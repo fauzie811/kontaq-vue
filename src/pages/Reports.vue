ddf383f7c1f080cf3a0d0e3ba6fd89d7c32915f9
Fauzie <fauzie811@yahoo.com>
2023-06-10 10:02:47 +0700

    fix: report share

--- src/pages/Reports.vue
+++ src/pages/Reports.vue
@@ -57,7 +57,7 @@
 
     <div v-if="reports" class="flex justify-end mt-4">
       <button @click="shareDialog = true" type="button"
-        class="px-3 py-2 text-sm font-semibold text-white bg-lime-600 rounded-md shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600">Bagikan</button>
+        class="px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-lime-600 hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600">Bagikan</button>
     </div>
 
     <TransitionRoot as="template" :show="shareDialog">
@@ -89,11 +89,11 @@
                       <br />
                       📋 Kuis wajib dikerjakan<br />
                       <br />
-                      🆔 1️⃣2️⃣3️⃣4️⃣5️⃣6⃣<br />
+                      🆔 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ <br />
                       <br />
                       <span v-for="(item, idx) in reports.items">
                         {{ `${idx + 1}`.padStart(2, '0') }}
-                        <span v-for="quiz in reports.quizzes" :key="quiz.id">
+                        <span class="text-red-600" v-for="quiz in reports.quizzes" :key="quiz.id">
                           {{ quizScoreEmoji(item.scores ? item.scores[`quiz_${quiz.id}`] : undefined) }}
                         </span>
                         <span v-for="evaluation in reports.evaluations" :key="evaluation.id">
@@ -118,7 +118,7 @@
                 </div>
                 <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                   <button type="button"
-                    class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-lime-600 rounded-md shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 sm:col-start-2"
+                    class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-lime-600 hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 sm:col-start-2"
                     @click="shareDialog = false">Tutup</button>
                   <button type="button"
                     class="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
@@ -201,10 +201,11 @@ const calculateTotals = () => {
 };
 
 const quizScoreEmoji = (score) => {
-  if (score === null || score === undefined) return '✖️';
-  if (score == 'i') return 'ℹ️';
-  if (score == 'n') return '🆕';
-  return '💯';
+  if (score === null || score === undefined) return '✖️ ';
+  if (score == 'i') return 'ℹ️ ';
+  if (score == 'n') return '🆕 ';
+  if (score == 100) return '💯 ';
+  return score + ' ';
 }
 
 const evaluationScoreEmoji = (score) => {
